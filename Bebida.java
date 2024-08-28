3aad004da9969cecc78961684da2cb7aba604924
GabiEweldt <136033205+Gabiminius@users.noreply.github.com>
2024-08-28 11:45:00 -0400

    Add files via upload

---
+++ Bebida.java
@@ -0,0 +1,16 @@
+package lab3;
+
+public class Bebida {
+    public static final int  VOLUMEN=500;
+    private int contenido;
+    private int serie;
+    public Bebida(int sorbos){}
+    public int getSerie(){
+        return serie;
+    }
+    public void llenar(){
+    }
+    public llenar(){}
+
+
+}
